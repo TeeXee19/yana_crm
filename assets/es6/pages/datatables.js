101d89e45b903b1fe0fcea9a35fc44a6e3de4454
Tochukwu Franklin Ene <teec3000@gmail.com>
2021-04-08 13:00:40 +0100

    Worked on the Create Customer Page and the View User Page

--- assets/es6/pages/datatables.js
+++ assets/es6/pages/datatables.js
@@ -1,7 +1,9 @@
 class TablesDataTable {
 
     static init() {
-        $('#data-table').DataTable();
+        $('#data-table').DataTable({
+        	"lengthMenu": [[5,10, 25, 50, -1], [5,10, 25, 50, "All"]]
+        });
     }
 }
 
